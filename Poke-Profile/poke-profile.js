5de20d09a6d2b70aa8246959ecc07902e58855bd
Erick Molina <erick.k.molina@hotmail.com>
2022-02-28 02:40:21 -0600

    commit

---
+++ Poke-Profile/poke-profile.js
@@ -0,0 +1,30 @@
+function turnred(element){
+    element.classList.add("turnred")
+}
+function removered(element){
+    element.classList.remove("turnred")
+}
+function turnblue(element){
+    element.classList.add("turnblue")
+}
+function removeblue(element){
+    element.classList.remove("turnblue")
+}
+function turngreen(element){
+    element.classList.add("turngreen")
+}
+function removegreen(element){
+    element.classList.remove("turngreen")
+}
+function turnpurple(element){
+    element.classList.add("turnpurple")
+}
+function removepurple(element){
+    element.classList.remove("turnpurple")
+}
+function play(element){
+    element.play()
+}
+function play(element){
+    element.pause()
+}
